921a0232634995dfef6fc555d03516cda78e1b56
Ashley <mayashley052@gmail.com>
2020-06-09 17:37:52 -0400

    refactor to a class based component for logic

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import React from 'react';
 
 
 function App() {
+  // MDN geolocation api-current location
+  navigator.geolocation.getCurrentPosition(
+    // success callback that gets called when it goes right
+    (position) => console.log(position),
+    // failure callback when it cant get location
+    (err) => console.log(err)
+  )
   return (
-    <div className="App">
-     hello
+    <div>
+    Latitude:
     </div>
   );
 }
---
+++ src/Components/SeasonDisplay.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+ const SeasonDisplay = () => {
+    return (
+        <div>
+            Season Sisplay here
+        </div>
+    )
+}
+export default SeasonDisplay;
