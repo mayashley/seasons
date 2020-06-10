3aab93ccac30b0f653dd0faf3fd0b0cbfb120c18
Ashley <mayashley052@gmail.com>
2020-06-10 16:53:19 -0400

    getting geolocation of device for seasons app. latitude

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-import React from 'react';
+import React, { Component } from "react";
 
-
-function App() {
-  // MDN geolocation api-current location
-  navigator.geolocation.getCurrentPosition(
-    // success callback that gets called when it goes right
-    (position) => console.log(position),
-    // failure callback when it cant get location
-    (err) => console.log(err)
-  )
-  return (
-    <div>
-    Latitude:
-    </div>
-  );
+class App extends Component {
+  render() {
+    // MDN geolocation api-current location
+    navigator.geolocation.getCurrentPosition(
+      // success callback that gets called when it goes right
+      (position) => console.log(position),
+      // failure callback when it cant get location
+      (err) => console.log(err)
+    );
+    return <div>Latitude:</div>;
+  }
 }
 
 export default App;
