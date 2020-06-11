fa80cada44b1351caf509ac6fa9776280c6667bc
Ashley <mayashley052@gmail.com>
2020-06-11 15:16:13 -0400

    worked on location

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React, { Component } from "react";
 
 class App extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null };
+
     // MDN geolocation api-current location
     navigator.geolocation.getCurrentPosition(
       // success callback that gets called when it goes right
-      (position) => console.log(position),
+      (position) => {
+        // got this info from the console for gelocation info with a console.log
+        // and we called setState here to do it
+        this.setState({lat: position.coords.latitude });
+      },
       // failure callback when it cant get location
-      (err) => console.log(err)
+      (err) => {
+        alert("please refresh page and allow location");
+      }
     );
-    return <div>Latitude:</div>;
+  }
+
+  render() {
+    // reference the state below for the latitude
+    return <div>Latitude: {this.state.lat}</div>;
   }
 }
 
