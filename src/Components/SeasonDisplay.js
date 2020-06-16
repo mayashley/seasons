7ef47c66f1822ae3fe9ff5dc9485fc9a4d88488d
Ashley <mayashley052@gmail.com>
2020-06-16 15:14:15 -0400

    semantic icons, css and conditonal rendering

--- src/Components/SeasonDisplay.js
+++ src/Components/SeasonDisplay.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react";
+import "../SeasonDisplay.css";
 
- const SeasonDisplay = (props) => {
-    //  console.log(props.lat);
-    return (
-        <div>
-            Season Display here
-        </div>
-    )
-}
+const seasonConfig = {
+  summer: {
+    text: "It is Summer",
+    iconName: "sun",
+  },
+  winter: {
+    text: "It is Winter",
+    iconName: "snowflake",
+  },
+};
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    // turranary expression opperator
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat < 0 ? "winter" : "summer";
+  }
+};
+
+const SeasonDisplay = (props) => {
+  //  console.log(props.lat);
+  const season = getSeason(props.lat, new Date().getMonth());
+  // console.log(season);
+  const { text, iconName } = seasonConfig[season];
+  return (
+    <div className={`season-display ${season}`}>
+      <i className={`${iconName} icon massive icon-left`} />
+
+      <h1>{text}</h1>
+      <i className={`${iconName} icon huge icon-right`} />
+    </div>
+  );
+};
 export default SeasonDisplay;
